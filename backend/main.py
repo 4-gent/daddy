7b51370ece6ebaf9372e388bdf47b32ff52dcdcf
4-gent <spicyrice09@gmail.com>
2024-10-04 16:23:10 -0700

    overhaul of site

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,14 @@
-from flask import Flask, request, jsonify  # Import Flask and related modules for handling requests and responses
+from flask import Flask, request, jsonify, session  # Import Flask and related modules for handling requests and responses
 from flask_cors import CORS  # Import CORS for handling Cross-Origin Resource Sharing
+from flask_session import Session  # Import Session for handling user sessions
 from flask_bcrypt import Bcrypt  # Import Bcrypt for hashing passwords
-from connect import db, users  # Import database connection and users collection
+from connect import db, users, input_db, responses  # Import database connection and users collection
 
 app = Flask(__name__)  # Create a Flask application instance
 CORS(app, origins=['http://localhost:3000'])  # Enable CORS for the specified origin
 
 bcrypt = Bcrypt(app)  # Initialize Bcrypt with the Flask app
+Session(app)
 
 @app.route('/login', methods=['POST'])  # Define the login route with GET and POST methods
 def login():
@@ -15,6 +17,7 @@ def login():
         user = users.find_one({'username': data['username']})  # Find the user in the database by username
 
         if user and bcrypt.check_password_hash(user['password'], data['password']):  # Check if user exists and password matches
+            session['_id'] = str(user['_id'])  # Set the session ID to the user ID
             return jsonify({'message': 'Login successful'}), 200  # Return success message with status code 200
         else:
             return jsonify({'message': 'Invalid username or password'}), 401  # Return failure message with status code 401
@@ -28,7 +31,12 @@ def register():
         
         new_user = {  # Create a new user dictionary with hashed password
             'username': data['username'],
-            'password': bcrypt.generate_password_hash(data['password']).decode('utf-8')
+            'password': bcrypt.generate_password_hash(data['password']).decode('utf-8'),
+            'first_name': data['firstname'],
+            'last_name': data['lastname'],
+            'email': data['email'],
+            'parent': data['parent'],
+            'age': data['age']
         }
 
         if users.find_one({'username': data['username']}) is None:  # Check if the username is not already taken
@@ -38,6 +46,20 @@ def register():
             return jsonify({'message': 'Username already exists'}), 409  # Return conflict message with status code 409
     else:
         return jsonify({'message': 'Method not allowed'}), 405  # Return method not allowed message with status code 405
+
+@app.route('/input', methods=['GET', 'POST'])
+def input():
+    if request.method == 'POST':
+        
+    else:
+        return jsonify({'message': 'Server-side Error'}), 405
+
+@app.route('/api/response', methods=['GET', 'POST']):
+def response():
+    if request.method == 'POST':
+        
+    else:
+        return jsonify({'message': 'Server-side Error'}), 405
 
 if __name__ == '__main__':  # Check if the script is run directly (not imported)
     app.run(debug=True, port=4000)  # Run the Flask app in debug mode on port 4000
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,26 +2,47 @@
 import React from 'react'
 import './styles/main.css'
 // Importing the Navigation component from the components/navbar file
-import Navigation from './components/navbar'
+import Carousel from 'react-bootstrap/carousel'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import asian from './styles/asian.webp'
+import black from './styles/black.webp'
+import mexican from './styles/mexican.webp'
+import logo from './styles/logo.png'
 
 // Exporting the Main function as the default export
 export default function Main() {
     // Returning the JSX to be rendered
     return (
         <div className='main-body'>
-            {/* Navigation bar section */}
-            <nav>
-                {/* Rendering the Navigation component */}
-                <Navigation />
-            </nav>
-            {/* Main content section with flexbox layout */}
-            <div className='main-container d-flex flex-row justify-content-center'>
-                <div className='d-flex flex-column align-items-center'>
-                    {/* Main heading */}
-                    <h1>Welcome to Mj's Web Based Template "Copyrighted by Mj jus cuz"</h1>
-                    <br />
-                    {/* Description paragraph */}
-                    <p>This is a template application to get you started, change it however you see fit.</p>
+            <div className='d-flex flex-row justify-content-between'>
+                <Carousel fade={true} controls={false} interval={3000} indicators={false}>
+                    <Carousel.Item>
+                        <img
+                            className="carousel-img d-block"
+                            src={asian}
+                            alt="First slide"
+                        />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="carousel-img d-block"
+                            src={black}
+                            alt="Second slide"
+                        />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="carousel-img d-block"
+                            src={mexican}
+                            alt="Third slide"
+                        />
+                    </Carousel.Item>
+                </Carousel>
+                <div className='main-button-container d-flex flex-column align-items-center'>
+                    {/* <h1 className='main-title'>Daddy's Home</h1> */}
+                    <img className="main-logo" src={logo} />
+                    <a href='/login'><button className='main-button'>Login</button></a>
+                    <a href='/register'><button className='main-button'>Register</button></a>
                 </div>
             </div>
         </div>
